d0023ff1729557494b5003beeaa54c76ebd8cc4d
prof-gnoni <84866594+prof-gnoni@users.noreply.github.com>
2025-12-11 01:16:57 +0100

    Add files via upload
    Versione del database che prevede il login

--- database/first_db_usage.sql
+++ database/first_db_usage.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.1
+-- version 5.2.3
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:8889
--- Creato il: Nov 19, 2025 alle 01:01
+-- Creato il: Dic 11, 2025 alle 01:13
 -- Versione del server: 5.7.44
--- Versione PHP: 8.3.14
+-- Versione PHP: 7.4.33
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -20,8 +20,29 @@ SET time_zone = "+00:00";
 --
 -- Database: `first_db_usage`
 --
+DROP DATABASE IF EXISTS `first_db_usage`;
 CREATE DATABASE IF NOT EXISTS `first_db_usage` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
 USE `first_db_usage`;
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `gruppo`
+--
+
+CREATE TABLE `gruppo` (
+  `idGruppo` int(11) NOT NULL,
+  `nome` varchar(50) NOT NULL,
+  `descrizione` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dump dei dati per la tabella `gruppo`
+--
+
+INSERT INTO `gruppo` (`idGruppo`, `nome`, `descrizione`) VALUES
+(1, 'Amministratori', 'Accesso completo: gestione utenti, modifica e cancellazione'),
+(2, 'Standard', 'Accesso limitato: sola visualizzazione');
 
 -- --------------------------------------------------------
 
@@ -38,21 +59,47 @@ CREATE TABLE `utente` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Svuota la tabella prima dell'inserimento `utente`
---
-
-TRUNCATE TABLE `utente`;
---
 -- Dump dei dati per la tabella `utente`
 --
 
 INSERT INTO `utente` (`idUtente`, `nome`, `email`, `genere`, `dataNascita`) VALUES
-(4, 'akusdhvb', 't@k.j', 'donna', '2011-11-11'),
-(5, 'dadsasdas', 'r@hh.iu', 'uomo', '1975-04-10');
+(2, 'Emanuele', 'gnoni@istitutolevi.edu.it', 'uomo', '1975-04-10'),
+(3, 'Natascia', 'natascia@istitutolevi.edu.it', 'donna', '1975-06-18'),
+(4, 'Leonardo', 'leonardo@istitutolevi.edu.it', 'uomo', '1999-11-11'),
+(5, 'Giovanna', 'giovanna@istitutolevi.edu.it', 'donna', '1975-04-10'),
+(6, 'Pippo', 'pippo@disneyland.net', 'uomo', '2025-12-08');
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `utenti_sistema`
+--
+
+CREATE TABLE `utenti_sistema` (
+  `id` int(11) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `idGruppo` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dump dei dati per la tabella `utenti_sistema`
+--
+
+INSERT INTO `utenti_sistema` (`id`, `username`, `password`, `idGruppo`) VALUES
+(1, 'admin', '$2y$12$efKuOAY7U/m.zY8e7JOu9eNQgzQc9dU/87n1wyWCC68sVQhcsTfrm', 1),
+(2, 'user', '$2y$12$qKKNeNHhOIpqDafRBDytFeI//soCLPk8zZ2.E41qNY/7EbGU/LVsS', 2);
 
 --
 -- Indici per le tabelle scaricate
 --
+
+--
+-- Indici per le tabelle `gruppo`
+--
+ALTER TABLE `gruppo`
+  ADD PRIMARY KEY (`idGruppo`),
+  ADD UNIQUE KEY `nome` (`nome`);
 
 --
 -- Indici per le tabelle `utente`
@@ -61,14 +108,44 @@ ALTER TABLE `utente`
   ADD PRIMARY KEY (`idUtente`);
 
 --
+-- Indici per le tabelle `utenti_sistema`
+--
+ALTER TABLE `utenti_sistema`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `username` (`username`),
+  ADD KEY `fk_gruppo` (`idGruppo`);
+
+--
 -- AUTO_INCREMENT per le tabelle scaricate
 --
+
+--
+-- AUTO_INCREMENT per la tabella `gruppo`
+--
+ALTER TABLE `gruppo`
+  MODIFY `idGruppo` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT per la tabella `utente`
 --
 ALTER TABLE `utente`
-  MODIFY `idUtente` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `idUtente` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT per la tabella `utenti_sistema`
+--
+ALTER TABLE `utenti_sistema`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- Limiti per le tabelle scaricate
+--
+
+--
+-- Limiti per la tabella `utenti_sistema`
+--
+ALTER TABLE `utenti_sistema`
+  ADD CONSTRAINT `fk_gruppo` FOREIGN KEY (`idGruppo`) REFERENCES `gruppo` (`idGruppo`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
